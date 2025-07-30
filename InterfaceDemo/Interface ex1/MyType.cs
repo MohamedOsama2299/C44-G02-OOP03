21941adfa019fec74c823a3121debcf12fa5afba
mohammedosamahelmy5 <mohammedosamahelmy5@outlook.com>
2025-07-30 15:47:08 +0300

    InterfaceDemo

---
+++ InterfaceDemo/Interface ex1/MyType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo.Interface_ex1
+{
+    internal class MyType:  Program ,IMyType
+    {
+        public double salary { get ; set ; }
+        public void Myfun()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
