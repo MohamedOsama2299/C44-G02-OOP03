1a01edc447c6e3e86b8a3c9f71921f8e7f2aadba
mohammedosamahelmy5 <mohammedosamahelmy5@outlook.com>
2025-07-30 19:20:20 +0300

    Assignment3

---
+++ InterfaceDemo/Question 02/client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo.Question_02
+{
+    internal class Client : Users
+    {
+        Users[] users;
+        public Client()
+        {
+
+        }
+    }
+}
---
+++ InterfaceDemo/Question01/IRectangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo.Question01
+{
+    internal interface IRectangle
+    {
+        double Length { get; set; }
+        double Width { get; set; }
+        void CalculateArea();
+    }
+}
